d53604f1d340130cc509435a5ceab93d90f17bcb
Forrest Miller <Forrestmillerj@gmail.com>
2020-03-18 20:30:07 -0500

    Serves index.html at '/*'

--- Develop/app.js
+++ Develop/app.js
@@ -25,6 +25,10 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 })
 
+app.get('*', function(req,res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
 
 // Starts the server to begin listening
 // =============================================================
