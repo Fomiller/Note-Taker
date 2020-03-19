cfde0b8166aa45cf13d94d73e784c5b7429d3497
Forrest Miller <Forrestmillerj@gmail.com>
2020-03-18 20:16:34 -0500

    Server listening at PORT:8000. Server serving index.html at '/'.

---
+++ Develop/app.js
@@ -0,0 +1,28 @@
+// Dependencies
+// ==========================================================
+const express = require("express");
+const path = require("path");
+
+// Sets up the express app
+// ==========================================================
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// Routes
+// ==================================================================
+
+// Basic route that send the user first to the AJAZ Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+})
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening at http://localhost:" + PORT);
+})
